8b9d9a1d226ff864a4f2772a0d1ca947e1d639af
Kah Wei <xeclipse120@live.com>
2016-06-02 10:34:54 +0800

    Added multi-light

--- scripts/light.js
+++ scripts/light.js
@@ -4,25 +4,95 @@
 function Light()
 {
     // Basics
+	this.enabled = false;
     this.type = Light.TYPE_POINT;
     this.position = vec3.create();
     this.direction = vec3.create();
-    
+
     // Lighting Coefficients
     this.kAmbient = 0.5;
     this.kDiffuse = 0.5;
     this.kSpecular = 0.5;
-    
+
     // Attenuation
     this.kConstant = 1.0;
     this.kLinear = 0.001;
     this.kQuadratic = 0.00002;
-    
+
     // Spot Light Angles
     this.spotInnerAngle = 30.0;
     this.spotOuterAngle = 120.0;
+
+	// Function Definitions
+	this.Init = Initialize;
+	this.InitPoint = InitializePoint;
+	this.InitDirectional = InitializeDirectional;
+	this.InitSpot = InitializeSpot;
+	this.SetLightProperties = SetLightProperties;
+	this.SetAttenuation = SetAttenuation;
 }
 
 Light.TYPE_POINT = 0;
 Light.TYPE_DIRECTIONAL = 1;
 Light.TYPE_SPOT = 2;
+
+// Function to initialize the light basics
+// -- type : int (0 - 2) (Light.TYPE_POINT, Light.TYPE_DIRECTIONAL, Light.TYPE_SPOT)
+// -- position: vec3
+function Initialize(lightType, position)
+{
+	this.type = lightType;
+	vec3.set(this.position, position);// = vec3.copy(position);
+	this.enabled = true;
+}
+
+// Function to initialize the light as Point
+// -- position: vec3
+function InitializePoint(position)
+{
+	Initialize(Light.TYPE_POINT, position);
+}
+
+// Function to initialize the light as Directional
+// -- position: vec3
+// -- direction : vec3
+function InitializeDirectional(position, direction)
+{
+	Initialize(Light.TYPE_DIRECTIONAL, position);
+	this.direction = direction;
+}
+
+// Function to initialize the light as
+// -- position: vec3
+// -- direction : vec3
+// -- innerAngle: float
+// -- outerAngle: float
+function InitializeSpot(position, direction, innerAngle, outerAngle)
+{
+	Initialize(Light.TYPE_SPOT, position);
+	this.direction = direction;
+	this.innerAngle = innerAngle;
+	this.outerAngle = outerAngle;
+}
+
+// Function to set the light properties
+// -- ambient: float
+// -- diffuse: float
+// -- specular: float
+function SetLightProperties(ambient, diffuse, specular)
+{
+	this.kAmbient = ambient;
+	this.kDiffuse = diffuse;
+	this.kSpecular = specular;
+}
+
+// Function to set the degree of attenuation
+// -- constant: float
+// -- linear: float
+// -- quadratic: float
+function SetAttenuation(constant, linear, quadratic)
+{
+	this.kConstant = constant;
+	this.kLinear = linear;
+	this.kQuadratic = quadratic;
+}
--- scripts/webGL.js
+++ scripts/webGL.js
@@ -1,4 +1,9 @@
 /*
+ * Constants
+ */
+const MAX_LIGHTS = 8;
+
+/*
  * Variable Declaration and Definitions
  */
 var timeSinceStart = 0.0;           // The time since the simulations tarted
@@ -16,18 +21,18 @@ var mMatrix = mat4.create();        // Model Matrix
 /*
  * Callback Code
  */
-window.requestAnimFrame = 
+window.requestAnimFrame =
 (
     function()
     {
-        return window.requestAnimationFrame 
-        || 
+        return window.requestAnimationFrame
+        ||
         window.webkitRequestAnimationFrame
-        || 
+        ||
         window.mozRequestAnimationFrame
-        || 
+        ||
         window.oRequestAnimationFrame
-        || 
+        ||
         window.msRequestAnimationFrame
         ||
         function(callback, element)
@@ -71,10 +76,10 @@ function Draw()
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     // Clear the color as well as the depth buffer.
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
+
     // Specify which shader to use
     gl.useProgram(shaderProgram);
-    
+
     // Get the attributes references from shader
     // -- Position
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vPos");
@@ -88,32 +93,52 @@ function Draw()
     // -- Normal
     shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "vNorm");
     gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
-    
+
     // Get the uniforms from the shader
     // -- MVP
     shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
     shaderProgram.vMatrixUniform = gl.getUniformLocation(shaderProgram, "uVMatrix");
-    shaderProgram.mMatrixUniform = gl.getUniformLocation(shaderProgram, "uMMatrix");    
+    shaderProgram.mMatrixUniform = gl.getUniformLocation(shaderProgram, "uMMatrix");
     shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
     // -- Camera
     shaderProgram.cameraView = gl.getUniformLocation(shaderProgram, "viewDirection");
-    // -- Lights
-    shaderProgram.lightType = gl.getUniformLocation(shaderProgram, "light[0].type");
-    shaderProgram.lightPos = gl.getUniformLocation(shaderProgram, "light[0].position");
-    shaderProgram.lightDir = gl.getUniformLocation(shaderProgram, "light[0].direction");
-    shaderProgram.kAmbient = gl.getUniformLocation(shaderProgram, "light[0].kAmbient");
-    shaderProgram.kDiffuse = gl.getUniformLocation(shaderProgram, "light[0].kDiffuse");
-    shaderProgram.kSpecular = gl.getUniformLocation(shaderProgram, "light[0].kSpecular");
-    shaderProgram.kConstant = gl.getUniformLocation(shaderProgram, "light[0].kConstant");
-    shaderProgram.kLinear = gl.getUniformLocation(shaderProgram, "light[0].kLinear");
-    shaderProgram.kQuadratic = gl.getUniformLocation(shaderProgram, "light[0].kQuadratic");
-    shaderProgram.spotInnerAngle = gl.getUniformLocation(shaderProgram, "light[0].spotInnerAngle");
-    shaderProgram.spotOuterAngle = gl.getUniformLocation(shaderProgram, "light[0].spotOuterAngle");
     // -- Material
     shaderProgram.diffuseColor = gl.getUniformLocation(shaderProgram, "diffuseColor");
     shaderProgram.ambientColor = gl.getUniformLocation(shaderProgram, "ambientColor");
     shaderProgram.specularColor = gl.getUniformLocation(shaderProgram, "specularColor");
     shaderProgram.shininess = gl.getUniformLocation(shaderProgram, "shininess");
+
+    // Lights
+	// -- Loop through all the lights
+    for (var i = 0; i < MAX_LIGHTS && i < lightList.length; ++i)
+    {
+        // -- Get Uniforms
+		shaderProgram.lightEnabled = gl.getUniformLocation(shaderProgram, "light[" + i + "].enabled");
+        shaderProgram.lightType = gl.getUniformLocation(shaderProgram, "light[" + i + "].type");
+        shaderProgram.lightPos = gl.getUniformLocation(shaderProgram, "light[" + i + "].position");
+        shaderProgram.lightDir = gl.getUniformLocation(shaderProgram, "light[" + i + "].direction");
+        shaderProgram.kAmbient = gl.getUniformLocation(shaderProgram, "light[" + i + "].kAmbient");
+        shaderProgram.kDiffuse = gl.getUniformLocation(shaderProgram, "light[" + i + "].kDiffuse");
+        shaderProgram.kSpecular = gl.getUniformLocation(shaderProgram, "light[" + i + "].kSpecular");
+        shaderProgram.kConstant = gl.getUniformLocation(shaderProgram, "light[" + i + "].kConstant");
+        shaderProgram.kLinear = gl.getUniformLocation(shaderProgram, "light[" + i + "].kLinear");
+        shaderProgram.kQuadratic = gl.getUniformLocation(shaderProgram, "light[" + i + "].kQuadratic");
+        shaderProgram.spotInnerAngle = gl.getUniformLocation(shaderProgram, "light[" + i + "].spotInnerAngle");
+        shaderProgram.spotOuterAngle = gl.getUniformLocation(shaderProgram, "light[" + i + "].spotOuterAngle");
+        // -- Define the values
+		gl.uniform1i(shaderProgram.enabled, lightList[i].enabled);
+        gl.uniform1i(shaderProgram.lightType, lightList[i].type);
+        gl.uniform3f(shaderProgram.lightPos, lightList[i].position[0], lightList[i].position[1], lightList[i].position[2]);
+        gl.uniform3f(shaderProgram.lightDir, lightList[i].direction[0], lightList[i].direction[1], lightList[i].direction[2]);
+        gl.uniform1f(shaderProgram.kAmbient, lightList[i].kAmbient);
+        gl.uniform1f(shaderProgram.kDiffuse, lightList[i].kDiffuse);
+        gl.uniform1f(shaderProgram.kSpecular, lightList[i].kSpecular);
+        gl.uniform1f(shaderProgram.kConstant, lightList[i].kConstant);
+        gl.uniform1f(shaderProgram.kLinear, lightList[i].kLinear);
+        gl.uniform1f(shaderProgram.kQuadratic, lightList[i].kQuadratic);
+        gl.uniform1f(shaderProgram.spotInnerAngle, lightList[i].spotInnerAngle);
+        gl.uniform1f(shaderProgram.spotOuterAngle, lightList[i].spotOuterAngle);
+    }
 
     // Defining Projection Matrix
     mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 10000.0, pMatrix);
@@ -121,66 +146,53 @@ function Draw()
     // Defining the View Matrix
     mat4.identity(vMatrix);
     mat4.translate(vMatrix, cameraPos);      // Camera Position
-    
+
     // Define the Camera View
     gl.uniform3f(shaderProgram.cameraView, cameraView[0], cameraView[1], cameraView[2]);
-    
-    // Define the light values
-    gl.uniform1i(shaderProgram.lightType, lightList[0].type);
-    gl.uniform3f(shaderProgram.lightPos, lightList[0].position[0], lightList[0].position[1], lightList[0].position[2]);
-    gl.uniform3f(shaderProgram.lightDir, lightList[0].direction[0], lightList[0].direction[1], lightList[0].direction[2]);
-    gl.uniform1f(shaderProgram.kAmbient, lightList[0].kAmbient);
-    gl.uniform1f(shaderProgram.kDiffuse, lightList[0].kDiffuse);
-    gl.uniform1f(shaderProgram.kSpecular, lightList[0].kSpecular);
-    gl.uniform1f(shaderProgram.kConstant, lightList[0].kConstant);
-    gl.uniform1f(shaderProgram.kLinear, lightList[0].kLinear);
-    gl.uniform1f(shaderProgram.kQuadratic, lightList[0].kQuadratic);
-    gl.uniform1f(shaderProgram.spotInnerAngle, lightList[0].spotInnerAngle);
-    gl.uniform1f(shaderProgram.spotOuterAngle, lightList[0].spotOuterAngle); 
-    
+
     // Drawing Objects
     for (var i = 0; i < drawList.length; ++i)
     {
         var mesh = drawList[i];
-        
+
         // Set the model transform matrix to be this object's transform
         mMatrix = mesh.transform
-        
+
         // Send vertex data into shader
         gl.bindBuffer(gl.ARRAY_BUFFER, mesh.vertexBuffer);
         gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-        
+
         // Send color data into shader
         // gl.bindBuffer(gl.ARRAY_BUFFER, mesh.colorBuffer);
         // gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
-    
+
         // Send texCoord data into shader
         gl.bindBuffer(gl.ARRAY_BUFFER, mesh.colorBuffer);
         gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, 2, gl.FLOAT, false, 0, 0);
-    
+
         // Bind the texture
         gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, mesh.tex);
         gl.uniform1i(shaderProgram.samplerUniform, 0);
-    
+
         // Bind the normals
         gl.bindBuffer(gl.ARRAY_BUFFER, mesh.normalBuffer);
         gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
-    
+
         // Send index data into shader
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mesh.indexBuffer);
-        
+
         // Set the matrix uniforms for MVP
         gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
         gl.uniformMatrix4fv(shaderProgram.vMatrixUniform, false, vMatrix);
         gl.uniformMatrix4fv(shaderProgram.mMatrixUniform, false, mMatrix);
-        
+
         // Set the lighting Material uniforms
         gl.uniform3f(shaderProgram.diffuseColor, mesh.material.diffuse[0], mesh.material.diffuse[1], mesh.material.diffuse[2]);
         gl.uniform3f(shaderProgram.ambientColor, mesh.material.ambient[0], mesh.material.ambient[1], mesh.material.ambient[2]);
         gl.uniform3f(shaderProgram.specularColor, mesh.material.specular[0], mesh.material.specular[1], mesh.material.specular[2]);
         gl.uniform1f(shaderProgram.shininess, mesh.material.shininess);
-        
+
         // Draw the objects
         gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
     }
@@ -188,8 +200,8 @@ function Draw()
 
 /*
  * GL Code
- */    
-function Setup() 
+ */
+function Setup()
 {
     // Get a handle to the canvas element
     var canvas = document.getElementById("glCanvas");
@@ -199,7 +211,7 @@ function Setup()
     {
         return;
     }
-    
+
     // Only continue if WebGL is available and working
     gl = (canvas.getContext("webgl")) ? canvas.getContext("webgl") : canvas.getContext("experimental-webgl");
     gl.viewportWidth = canvas.width;
@@ -207,34 +219,34 @@ function Setup()
 
     // Load Shaders
     shaderProgram = GetShaderProgram("VertexShader", "FragmentShader");
-    
+
     // Initialize Lights
     SetupLights();
-    
+
     // Load VBO, IBO, Color Buffers
     SetupBuffers();
 
     // Set the Camera Position
     cameraPos = [0.0, 0.0, -5.0];
     cameraView = [0.0, 0.0, 1.0];
-    
+
     // Render
     Tick();
 }
-    
+
 function GetShader(shaderID)
 {
     // Obtain the script element
     var script = document.getElementById(shaderID);
-    
+
     // Check if this script element exists
     if (!script)
     {
         alert("Unable to find the " + shaderID + " script element.");
-        
+
         return null;
-    }   
-    
+    }
+
     // Extract shader code from HTML
     var code = "";
     var currChild = script.firstChild;
@@ -244,13 +256,13 @@ function GetShader(shaderID)
         {
             code += currChild.textContent;
         }
-        
+
         currChild = currChild.nextSibling;
     }
-    
+
     // Variable to hold the shader
     var shader;
-    
+
     // Check the type of shader and generate the shader
     if (script.type == "x-shader/x-fragment")
     {
@@ -263,94 +275,101 @@ function GetShader(shaderID)
     else
     {
         alert("Unable to load a script of type " + script.type + ".");
-        
+
         return null;
     }
-    
+
     // Set the source for compiling the shader
     gl.shaderSource(shader, code);
     // Compile the shader
     gl.compileShader(shader);
-    
+
     // Check to see if compilation was ok
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) 
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
     {
           alert(gl.getShaderInfoLog(shader));
-          
+
           return null;
     }
-    
+
     return shader;
 }
-    
+
 function GetShaderProgram(vert, frag)
 {
     // Get the Shaders
     var vertexShader = GetShader(vert);
     var fragShader = GetShader(frag);
-    
+
     // Create a GL program
     var program = gl.createProgram();
-    
+
     // Attach the shaders to the GL program
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragShader);
-    
+
     // Link the program
     gl.linkProgram(program);
-    
+
     // Error Checking
     if (!gl.getProgramParameter(program, gl.LINK_STATUS))
     {
         alert("Could not initialize shaders");
     }
-    
+
     return program;
 }
 
 function SetupLights()
 {
-    // Define a light
-    var light = new Light();
-    
+    // Populate the list with all the lights
+    while (lightList.length < MAX_LIGHTS)
+	{
+		lightList.push(new Light());
+	}
+
     // Set it's params
-    light.position = [0, 10, -5];
-    
-    // Add it to the light list
-    lightList.push(light);
+    lightList[0].position = [0, 10, -5];
+	lightList[1].position = [0, 10, 5];
+    lightList[2].position = [-5, 10, 0];
+	lightList[3].position = [5, 10, 0];
+    lightList[4].position = [0, -10, -5];
+	lightList[5].position = [0, -10, 15];
+    lightList[6].position = [0, -10, 0];
+	lightList[7].position = [5, -10, 0];
 }
 
 function SetupBuffers()
 {
     var mesh = new Mesh();
     mesh.CreateCube();
-    
+
     // Create the Vertex Buffer and fill it up
     mesh.vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, mesh.vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mesh.vertices), gl.STATIC_DRAW);
-    
+
     // Create the Color Buffer and fill it up
     mesh.colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, mesh.colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mesh.colors), gl.STATIC_DRAW); 
-    
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mesh.colors), gl.STATIC_DRAW);
+
     // Create the Index Buffer and fill it up
     mesh.indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mesh.indexBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(mesh.indices), gl.STATIC_DRAW);
-    
+
     // Create the Normal Buffer and fill it up
     mesh.normalBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, mesh.normalBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mesh.normals), gl.STATIC_DRAW);
-    
+
     // Add a texture to it
     SetupTexture("images/cubeTex.png", mesh);
-    
+
     // Set the material
     mesh.material.diffuse = [1.0, 1.0, 1.0];
-    
+
     // Add this mesh to the list of objects to draw
     drawList.push(mesh);
 }
@@ -369,6 +388,6 @@ function SetupTexture(file, mesh)
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
         gl.bindTexture(gl.TEXTURE_2D, null);
     }
-    
+
     mesh.tex.image.src = file;
 }
